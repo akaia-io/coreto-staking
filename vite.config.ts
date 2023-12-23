34c1860e684ae2105783c6d2bd6237b7f0accc5c
Carina.Akaia.near <carina@dmail.ai>
2023-12-23 21:24:09 +0400

    Fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import preact from "@preact/preset-vite"
 import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
-import { chunkSplitPlugin } from "vite-plugin-chunk-split"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,24 +10,5 @@ export default defineConfig({
 		modulePreload: { polyfill: false },
 	},
 
-	plugins: [
-		UnoCSS(),
-		preact(),
-
-		chunkSplitPlugin({
-			strategy: "unbundle",
-
-			customChunk: ({ file: modulePath }) => {
-				const croppedPath = modulePath.replace(/(\.\.\/)+/, "")
-
-				if (/\.(ts|tsx|js|jsx)$/.test(modulePath)) {
-					return croppedPath.includes("node_modules")
-						? ["vendor", croppedPath.match(/node_modules\/(.*?)\/(.*?)@/).at(2)].join("/")
-						: croppedPath.replace(/src\//, "").replace(/\.(ts|tsx)$/, "")
-				} else {
-					return null
-				}
-			},
-		}),
-	],
+	plugins: [UnoCSS(), preact()],
 })
